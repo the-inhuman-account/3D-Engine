d8266c807aaf4f8551e69fa7aa5b070e2b1220c3
Arongil <lakernewhouse@gmail.com>
2018-12-05 12:09:19 -0800

    Create NPC class

---
+++ javascript/Player.js
@@ -0,0 +1,11 @@
+class NPC {
+
+  constructor(pos, size, texturePath) {
+    this.pos = pos;
+    this.vel = new Vector3D(0, 0, 0);
+    this.size = size; // Vector3D corresponding to the width, height, and length of the NPC. This doesn't rotate with the player, so it needs to be extra bulky.
+    this.collisionBox = new CollisionBox(this);
+    this.texturePath = texturePath; // file path for the texture image
+  }
+
+}
