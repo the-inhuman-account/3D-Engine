1900d258cd1e4525cb434eef9061f336e171b69a
Arongil <lakernewhouse@gmail.com>
2018-12-05 13:42:36 -0800

    Update CollisionBox.js

--- javascript/CollisionBox.js
+++ javascript/CollisionBox.js
@@ -3,8 +3,7 @@ class CollisionBox {
   constructor(parent) {
     // The collision box is from the center of the parent NPC and extends outward to make a box with dimension according to the size of the parent (x, y, z lengths).
     this.parent = parent; // The NPC for which the collision box is designed.
-    this.uncollideAccuracy = 0.8; // Number between 0 and 1 for the factor by which the velocity is decreased until a measure is found that "unstucks" the objects.
-    this.uncollideSteps = Math.ceil( Math.log(0.01) / Math.log(this.uncollideAccuracy) ); // 0.01 is chosen arbitrarily; it forces the program to check down to one one-hundredth of the velocity when unstucking.
+    this.uncollideSteps = 10; // The steps the CollisionBox will binary search to correctly "unstuck" the object.
   }
   
   static isInside(aPos, aSize, bPos, bSize) {
@@ -23,14 +22,6 @@ class CollisionBox {
     return this.parent.size;
   }
   
-  setUncollideAccuracy(value) {
-    if (value > 1 || value < 0) {
-      return;
-    }
-    this.uncollideAcurracy = value;
-    this.uncollideSteps = Math.ceil( Math.log(0.01) / Math.log(this.uncollideAccuracy) );
-  }
-  
   isInside(other) {
     return CollisionBox.isInside(this.pos, this.size, other.pos, other.size);
   }
@@ -40,18 +31,18 @@ class CollisionBox {
       return false;
     }
     // Reposition the NPC such that it is no longer inside the other object.
-    // Add parts of the negative of the NPC's velocity until the NPC are outside of each other.
-    for (var i = 0, factor = 1, oldPos = this.pos, newPos; i < this.uncollideSteps; i++) {
-      factor *= this.uncollideAccuracy;
-      newPos = this.pos.getShifted(this.parent.vel.getScaled(factor));
-      if (CollisionBox.isInside(newPos, this.size, other.pos, other.size)) {
-        // If the new position is inside the object, then use the old position, which is the closest we'll get to correct.
-        this.pos = oldPos;
-        return;
+    // Binary search the optimal distance to extract it: more than nothing, less than the entire negative velocity.
+    var high = 0, low = -1, mid, i;
+    for (i = 0; i < this.uncollideSteps; i++) {
+      mid = (high + low) / 2;
+      if (CollisionBox.isInside(this.pos.getShifted( this.parent.vel.getScaled(mid) ), this.size, other.pos, other.size)) {
+        high = mid; // If the middle box is still inside, then the box needs to go out farther.
+      }
+      else {
+        low = mid; // If the middle box is still inside, then the box needs to go out less.
       }
     }
-    // If the velocity is so great that we cannot find a slow enough multiple, return false.
-    return false;
+    this.pos.shift( this.parent.vel.getScaled( (high+low)/2 ) );
   }
 
 }
