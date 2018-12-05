5e76f0576006f8b459141ad0bad086c09c9ad019
Arongil <lakernewhouse@gmail.com>
2018-12-05 13:48:21 -0800

    Update NPC.js

--- javascript/NPC.js
+++ javascript/NPC.js
@@ -1,11 +1,17 @@
 class NPC {
 
-  constructor(pos, size, texturePath) {
+  constructor(GC, pos, size, texturePath) {
+    this.GC = GC; // the GameController
     this.pos = pos;
     this.vel = new Vector3D(0, 0, 0);
     this.size = size; // Vector3D corresponding to the width, height, and length of the NPC. This doesn't rotate with the player, so it needs to be extra bulky.
     this.collisionBox = new CollisionBox(this);
     this.texturePath = texturePath; // file path for the texture image
+  }
+  
+  physics() {
+    this.pos.shift(this.vel);
+    this.GC.checkCollisions(this);
   }
 
 }
