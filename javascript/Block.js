2ff0defca92c9aef6826630c4475d3201fb8f689
Arongil <lakernewhouse@gmail.com>
2018-12-05 13:59:37 -0800

    Create Block skeleton
    Block objects can be floors, walls, or anything else that players and NPCs need to collide with.

---
+++ javascript/Block.js
@@ -0,0 +1,17 @@
+class Block {
+
+  constructor(GC, pos, size, texture) {
+    this.GC = GC;
+    this.pos = pos;
+    this.size = size;
+    this.collisionBox = new CollisionBox(this);
+    this.texture = texture; // a file path leading to a texture
+  }
+  
+  display() {
+  }
+  
+  update() {
+  }
+
+}
